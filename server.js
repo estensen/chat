25dff8fe28a37d8b2efa2c198bdb1bf9d8222a2a
Håvard Anda Estensen <haavard.ae@gmail.com>
2016-09-17 11:38:17 +0200

    Add response

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 var net = require('net')
 
-var host = ''
+var host = '192.168.2.99'
 var port = 1337
-
-var user_ids = {}
-var messages = []
-var clients = []
 
 net.createServer(function (sock) {
   console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort)
@@ -15,6 +11,12 @@ net.createServer(function (sock) {
     content: 'Welcome! Type \'help\' to show commands.',
     server: 'server'
   }
-})
+  var sendString = JSON.stringify(intro)
+  sock.write(sendString)
+  sock.on('data', function (data) {
+    var message = JSON.parse(data)
+    console.log(message)
+  })
+}).listen(port, host)
 
 console.log('Server listening on ' + host + ':' + port)
