88d350a3037257aeef1f982f7fa077cf3e83d8cd
Håvard Anda Estensen <haavard.ae@gmail.com>
2016-09-17 14:53:41 +0200

    Replace var with let

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-var net = require('net')
+let net = require('net')
 
-var host = '192.168.2.99'
-var port = 1337
+let host = '192.168.2.99'
+let port = 1337
 
 net.createServer(function (sock) {
   console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort)
-  var intro = {
+  let intro = {
     response: 'info',
     timestamp: Date.now(),
     content: 'Welcome! Type \'help\' to show commands.',
     server: 'server'
   }
-  var sendString = JSON.stringify(intro)
+  let sendString = JSON.stringify(intro)
   sock.write(sendString)
 
   sock.on('data', function (data) {
-    var message = JSON.parse(data)
+    let message = JSON.parse(data)
     console.log(message)
 
     if (message.request === 'message') {
@@ -49,13 +49,13 @@ net.createServer(function (sock) {
   }
 
   function helpValidation () {
-    var response = {
+    let response = {
       timestamp: Date.now(),
       sender: 'server',
       response: 'info',
       content: 'LIST OF COMMANDS \nlogin <username> - logs you in \nlogout - logs you out \nmsg <message> - sends a message \nnames - lists users in chat \nhistory -  lists previous messages\nhelp - gives you this'
     }
-    var sendString = JSON.stringify(response)
+    let sendString = JSON.stringify(response)
     sock.write(sendString)
   }
 }).listen(port, host)
