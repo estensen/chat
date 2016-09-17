8fb012dd9a55d50255c20d36f7d8d1db8b1cfe41
Håvard Anda Estensen <haavard.ae@gmail.com>
2016-09-17 14:07:15 +0200

    Add helpValidation

--- server.js
+++ server.js
@@ -13,10 +13,30 @@ net.createServer(function (sock) {
   }
   var sendString = JSON.stringify(intro)
   sock.write(sendString)
+
   sock.on('data', function (data) {
     var message = JSON.parse(data)
     console.log(message)
-  })
+
+    if (message.request === 'message') {
+      messageValidation(message)
+    } else if (message.request === 'help') {
+      helpValidation()
+    } else {
+        // Add error
+    }
+    })
+
+    function helpValidation () {
+      var response = {
+        timestamp: Date.now(),
+        sender: 'server',
+        response: 'info',
+        content: 'LIST OF COMMANDS \nlogin <username> - logs you in \nlogout - logs you out \nmsg <message> - sends a message \nnames - lists users in chat \nhistory -  lists previous messages\nhelp - gives you this'
+      }
+      var sendString = JSON.stringify(response)
+      sock.write(sendString)
+    }
 }).listen(port, host)
 
 console.log('Server listening on ' + host + ':' + port)
