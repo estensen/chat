cf3678d1cfd63818cceeb9a5d4cda78e19143952
Håvard Anda Estensen <haavard.ae@gmail.com>
2016-09-17 11:27:02 +0200

    Update to Python 3 naming conventions

--- client.py
+++ client.py
@@ -8,34 +8,33 @@ from threading import Thread
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect socket to the port where the server is listening
-server_address = ('192.168.2.99', 80)
-print('connecting to %s port %s' % server_address)
-
-emptyRequest = {'request': '', 'content': ''}
+server_address = ('192.168.2.99', 1337)
+empty_request = {'request': '', 'content': ''}
 
 def read_socket():
+    print('Connecting to %s port %s' % server_address)
     sock.connect(server_address)
     while True:
         data = sock.recv(10000)
         if len(data) is not 0:
-            receivedDict = json.loads(data)
-            if receivedDict['response'] == 'info' or receivedDict['response'] == 'error':
-                print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(receivedDict['timestamp']/1000)) + ' - ' + '/SERVER/: ' + receivedDict['content'])
-            if receivedDict['response'] == 'message':
-                print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(receivedDict['timestamp']/1000)) + ' - ' + receivedDict['sender'] + ': ' + receivedDict['content'])
-            if receivedDict['response'] == 'history':
-                print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(receivedDict['timestamp']/1000)) + ' - History:')
-                for i, j in zip(receivedDict['content'][0::2], receivedDict['content'][1::2]):
+            received_dict = json.loads(data)
+            if received_dict['response'] == 'info' or received_dict['response'] == 'error':
+                print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(received_dict['timestamp']/1000)) + ' - ' + '/SERVER/: ' + received_dict['content'])
+            if received_dict['response'] == 'message':
+                print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(received_dict['timestamp']/1000)) + ' - ' + received_dict['sender'] + ': ' + received_dict['content'])
+            if received_dict['response'] == 'history':
+                print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(received_dict['timestamp']/1000)) + ' - History:')
+                for i, j in zip(received_dict['content'][0::2], received_dict['content'][1::2]):
                     print(i + ': ' + j)
 
 def read_input():
     while True:
-        data = raw_input()
+        data = input()
         if data[:5] == 'login':
             sock.send(process_login(data))
         elif data[:6] == 'logout':
             sock.send(process_logout(data))
-        elif data[:3] == 'message':
+        elif data[:7] == 'message':
             print(data)
             sock.send(process_message(data))
         elif data[:5] == 'names':
@@ -45,3 +44,23 @@ def read_input():
         else:
             data = 'help'
             sock.send(process_help(data))
+
+def process_message(data):
+    send_dict = {}
+
+    if len(data.split(' ')) > 2:
+        string_array = data.split(' ')
+        send_dict['request'] = string_array[0]
+        send_dict['content'] = ' '.join(string_array[1:])
+        return json.dumps(send_dict)
+    else:
+        return json.dumps(empty_request)
+
+try:
+    t = Thread(target = read_socket)
+    t.deamon = True
+    t.start()
+    read_input()
+finally:
+    print(sys.stderr, 'Closing socket')
+    sock.close()
